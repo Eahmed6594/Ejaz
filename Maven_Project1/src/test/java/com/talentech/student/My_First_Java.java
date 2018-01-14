609f3db182f13306278ae14e5f7aa509bd51d0b6
Eahme <Eahme@192.168.0.32>
2018-01-13 19:47:50 -0600

    version 01

---
+++ Maven_Project1/src/test/java/com/talentech/student/My_First_Java.java
@@ -0,0 +1,12 @@
+package com.talentech.student;
+
+public class My_First_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello World");
+	
+	}
+
+}
